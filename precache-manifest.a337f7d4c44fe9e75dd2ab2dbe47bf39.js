ab5d9f5f5dd9b7d07fde4a166e6f82e07e760e84
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-04-19 00:23:36 +0600

    Updates

---
+++ precache-manifest.a337f7d4c44fe9e75dd2ab2dbe47bf39.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/royaltybd/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5416da9d7a7288c43615",
+    "url": "/royaltybd/static/css/main.b3579e75.chunk.css"
+  },
+  {
+    "revision": "7b189dea204708024091",
+    "url": "/royaltybd/static/js/runtime~main.4aa2670e.js"
+  },
+  {
+    "revision": "6dcd55c1147dd0c1f453480492d35d6a",
+    "url": "/royaltybd/static/media/piggy-bank.6dcd55c1.svg"
+  },
+  {
+    "revision": "3bebde4f4747c72d871a",
+    "url": "/royaltybd/static/js/2.d5534972.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/royaltybd/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/royaltybd/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/royaltybd/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "5416da9d7a7288c43615",
+    "url": "/royaltybd/static/js/main.142236f8.chunk.js"
+  },
+  {
+    "revision": "376d226d8923393f603055703a1669ef",
+    "url": "/royaltybd/static/media/animat-sign-post-color.376d226d.gif"
+  },
+  {
+    "revision": "861bd3494e9757eaf90a1aff84655628",
+    "url": "/royaltybd/static/media/16541.861bd349.svg"
+  },
+  {
+    "revision": "c63f68415294721030bf248bb156e47f",
+    "url": "/royaltybd/static/media/animat-diamond-color.c63f6841.gif"
+  },
+  {
+    "revision": "f3538c572dbe25ee22bc3a3b9ae880fb",
+    "url": "/royaltybd/static/media/animat-compass-color.f3538c57.gif"
+  },
+  {
+    "revision": "1678a3ecfb27a09d69d5ed27bc3500eb",
+    "url": "/royaltybd/static/media/cash.1678a3ec.svg"
+  },
+  {
+    "revision": "3bebde4f4747c72d871a",
+    "url": "/royaltybd/static/css/2.610866b4.chunk.css"
+  },
+  {
+    "revision": "10f650d92ac55f5b431f9ba8328956bc",
+    "url": "/royaltybd/index.html"
+  }
+];
